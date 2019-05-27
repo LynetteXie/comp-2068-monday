48a9558051262abe6591d8bebd797e03b3b4e2b8
LynetteXie <miss.lynettexie@gmail.com>
2019-05-27 17:10:58 -0400

    Beginning of Lecture 4

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# comp-2068-monday
+# comp-2068-thursday
 A blogging platform
--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Hey  'dere world!`);
+  res.send(`Hey 'dere world!`);
 });
 
 app.get('/about', (req, res) => {
-    res.send(`I like long walks on the beach.`);
+  res.send(`I like long walks on the beach.`);
 });
 
-app.listen(4000, () => console.log('Listening on 4000'));
+const port = (process.env.PORT || 4000);
+app.listen(port, () => console.log(`Listening on ${port}`));
